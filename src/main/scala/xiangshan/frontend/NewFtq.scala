bbc46e5662c4f52e812e700f528e68b8f903df0a
dhaiwen <842385365@qq.com>
2024-08-21 16:13:42 +0800

    FTQ sram read enable clockgating correct

--- src/main/scala/xiangshan/frontend/NewFtq.scala
+++ src/main/scala/xiangshan/frontend/NewFtq.scala
@@ -493,7 +493,7 @@ class Ftq(parentName:String = "Unknown")(implicit p: Parameters) extends XSModul
   ftqPcMem.io.wdata.fromBranchPrediction(bpuInResp)
 
   //                                                            ifuRedirect + backendRedirect + commit
-  val ftqRedirectMem = Module(new SyncDataModuleTemplate(new FtqRedirectEntry, FtqSize, 1+1+1, 1, "FtqEntry"))
+  val ftqRedirectMem = Module(new SyncDataModuleTemplate(new FtqRedirectEntry, FtqSize, 1+1+1, 1, hasRen = true))
   // these info is intended to enq at the last stage of bpu
   ftqRedirectMem.io.wen(0) := io.fromBpu.resp.bits.lastStage.valid(dupForFtq)
   ftqRedirectMem.io.waddr(0) := io.fromBpu.resp.bits.lastStage.ftqIdx.value
@@ -514,7 +514,7 @@ class Ftq(parentName:String = "Unknown")(implicit p: Parameters) extends XSModul
   }
 
   //                                                            ifuRedirect + backendRedirect + commit
-  val ftbEntryMem = Module(new SyncDataModuleTemplate(new FTBEntry_FtqMem, FtqSize, 1+1+1, 1, "FtqEntry"))
+  val ftbEntryMem = Module(new SyncDataModuleTemplate(new FTBEntry_FtqMem, FtqSize, 1+1+1, 1, hasRen = true))
   ftbEntryMem.io.wen(0) := io.fromBpu.resp.bits.lastStage.valid(dupForFtq)
   ftbEntryMem.io.waddr(0) := io.fromBpu.resp.bits.lastStage.ftqIdx.value
   ftbEntryMem.io.wdata(0) := io.fromBpu.resp.bits.lastStageFtbEntry
@@ -815,7 +815,7 @@ class Ftq(parentName:String = "Unknown")(implicit p: Parameters) extends XSModul
   val ifuWbValid = pdWb.valid
   val ifuWbIdx = pdWb.bits.ftqIdx.value
   // read ports:                                                         commit update
-  val ftqPdMem = Module(new SyncDataModuleTemplate(new FtqPdEntry, FtqSize, 1, 1, "FtqPd"))
+  val ftqPdMem = Module(new SyncDataModuleTemplate(new FtqPdEntry, FtqSize, 1, 1, hasRen = true))
   ftqPdMem.io.wen.head := ifuWbValid
   ftqPdMem.io.waddr.head := pdWb.bits.ftqIdx.value
   ftqPdMem.io.wdata.head.fromPdWb(pdWb.bits)
@@ -841,6 +841,7 @@ class Ftq(parentName:String = "Unknown")(implicit p: Parameters) extends XSModul
     ifuWbPtr_write := ifuWbPtr + 1.U
   }
 
+  ftbEntryMem.io.ren.get.head := ifuWbValid
   ftbEntryMem.io.raddr.head := ifuWbIdx
   val hasFalseHit = WireInit(false.B)
   when (RegNext(hitPdValid)) {
@@ -886,8 +887,10 @@ class Ftq(parentName:String = "Unknown")(implicit p: Parameters) extends XSModul
    */
 
   // redirect read cfiInfo, couples to redirectGen s2
+  ftqRedirectMem.io.ren.get.init.last := backendRedirect.valid
   ftqRedirectMem.io.raddr.init.last := backendRedirect.bits.ftqIdx.value
 
+  ftbEntryMem.io.ren.get.init.last := backendRedirect.valid
   ftbEntryMem.io.raddr.init.last := backendRedirect.bits.ftqIdx.value
 
   val stage3CfiInfo = ftqRedirectMem.io.rdata.init.last
@@ -930,8 +933,10 @@ class Ftq(parentName:String = "Unknown")(implicit p: Parameters) extends XSModul
   val ifuRedirectToBpu = WireInit(ifuRedirectReg)
   ifuFlush := fromIfuRedirect.valid || ifuRedirectToBpu.valid
 
+  ftqRedirectMem.io.ren.get.head := fromIfuRedirect.valid
   ftqRedirectMem.io.raddr.head := fromIfuRedirect.bits.ftqIdx.value
 
+  // ftbEntryMem.io.ren.get.head := true.B
   ftbEntryMem.io.raddr.head := fromIfuRedirect.bits.ftqIdx.value
 
   val toBpuCfi = ifuRedirectToBpu.bits.cfiUpdate
@@ -1076,13 +1081,16 @@ class Ftq(parentName:String = "Unknown")(implicit p: Parameters) extends XSModul
     Mux(RegNext(commPtr === newestEntryPtr),
       RegEnable(newestEntryTarget, newest_entry_target_modified),//RegNext(newestEntryTarget),
       RegNext(ftqPcMem.io.commPtrPlus1_rdata.startAddr))
+  ftqPdMem.io.ren.get.last := canCommit
   ftqPdMem.io.raddr.last := commPtr.value
   val commitPd = ftqPdMem.io.rdata.last
+  ftqRedirectMem.io.ren.get.last := canCommit
   ftqRedirectMem.io.raddr.last := commPtr.value
   val commitSpecInfo = ftqRedirectMem.io.rdata.last
   ftqMetaSram.io.ren(0) := canCommit
   ftqMetaSram.io.raddr(0) := commPtr.value
   val commitMeta = ftqMetaSram.io.rdata(0).meta
+  ftbEntryMem.io.ren.get.last := canCommit
   ftbEntryMem.io.raddr.last := commPtr.value
   val commitFtbEntry = ftqMetaSram.io.rdata(0).ftb_entry
 
